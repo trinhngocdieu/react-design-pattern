a09aeee75a2969e19c09266c81dbd58e9a154003
Dieu <trinh.ngoc.dieu@moneyforward.co.jp>
2023-12-21 12:58:22 +0900

    chore: update README

--- hooks/dog/useDogPower.ts
+++ hooks/dog/useDogPower.ts
@@ -1,13 +1,14 @@
 import { useState, useMemo } from 'react';
 import { Dog } from "interfaces";
 
+const MAGIC_NUMBER = 9;
 const useDogPower = (dog : Dog) => {
   const [power, setPower] = useState(null);
   const { avgHeight, avgWeight } = dog;
 
   useMemo(() => {
     const calculatePower = () => {
-      const calculatedPower = avgHeight + avgWeight * 9;
+      const calculatedPower = avgHeight + avgWeight * MAGIC_NUMBER;
       setPower(calculatedPower);
     };
 
